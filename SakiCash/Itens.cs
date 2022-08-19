0a5771103363d66462e1245061ef11fc75d46700
Alan Mestezk <95321834+AlanMestezk@users.noreply.github.com>
2022-08-19 17:37:10 -0300

    estudando c# e .Net

---
+++ SakiCash/Itens.cs
@@ -0,0 +1,39 @@
+﻿
+
+namespace SakiCash {
+    internal class ItensConta {
+
+        public int Conta { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        public ItensConta() { }
+
+        public ItensConta(int conta, string titular) {
+            Conta = conta;
+            Titular = titular;
+        }
+
+        public ItensConta(int conta, string titular, double depositoInicial):this(conta, titular) {
+            Deposito(depositoInicial);
+        }
+
+        public void Deposito(double quantia) {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia) {
+            Saldo -= quantia + 5.0;
+        }
+
+        override public string ToString() {
+            Console.WriteLine();
+            Console.WriteLine("DADOS DA CONTA");
+            Console.WriteLine($"Conta: {Conta}");
+            Console.WriteLine($"Titular: {Titular}");
+            Console.WriteLine($"Saldo: {Saldo.ToString("f2")} R$ Dinheiros");
+            return $"";
+        }
+
+    }
+}
---
+++ SakiCash/Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+
+using System;
+
+namespace SakiCash {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            ItensConta dadosConta;
+
+            Console.WriteLine();
+            Console.Write("Digite o numero da conta => ");
+            int conta = int.Parse(Console.ReadLine());
+            Console.Write("Digite o titular da conta => ");
+            string titular = Console.ReadLine();
+            Console.WriteLine();
+
+
+            Console.Write("Deseja digitar o depoisito inicial S/N?: ");
+            string resposta = Console.ReadLine();
+            if (resposta == "s" || resposta == "S" ) {
+                Console.WriteLine();
+                Console.Write("Digite o valor que deseja depositar: ");
+                double saldo = double.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+                dadosConta = new ItensConta (conta, titular, saldo);
+            }
+            else {
+                dadosConta = new ItensConta(conta, titular);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine(dadosConta);
+
+            Console.WriteLine();
+            Console.Write("Digite um valor para déposito: ");
+            double deposito = double.Parse(Console.ReadLine());
+            dadosConta.Deposito(deposito);
+            Console.WriteLine();
+            Console.WriteLine("O saldo da conta foi atualizado :)");
+            Console.WriteLine(dadosConta);
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Digite um valor para saque: ");
+            deposito = double.Parse(Console.ReadLine());
+            dadosConta.Saque(deposito);
+            Console.WriteLine();
+            Console.WriteLine("O saldo da conta foi atualizado :)");
+            Console.WriteLine(dadosConta);
+
+            Console.WriteLine();
+
+        }
+
+
+    }
+
+}
